d66cc8ee798a753e5a709cafb239370a32bcc19a
Leonard Papenmeier <leonard.papenmeier@posteo.de>
2022-12-03 12:26:36 -0600

    add more benchmarks

--- scalable_gps/objective.py
+++ scalable_gps/objective.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Optional
+
 import torch
 import numpy as np
-from botorch.test_functions import Ackley as _Ackley
+from botorch.test_functions import Ackley as _Ackley, Griewank as _Griewank
 from botorch.utils.transforms import unnormalize
+
+from scalable_gps.benchmark_utils import RoverDomain, create_large_domain
 
 
 def eval_objective(x, fun):
@@ -50,3 +54,58 @@ class Ackley(OptimizationProblem):
 
     def dim(self) -> int:
         return self._dim
+
+
+class Griewank(OptimizationProblem):
+
+    def __init__(self, dim: int):
+        self._dim = dim
+        self._griewank = _Griewank(dim=dim, negate=True)
+
+    def __call__(self, x: torch.Tensor):
+        return eval_objective(x, self._griewank)
+
+    def lb(self) -> np.ndarray:
+        return self._griewank.bounds[0]
+
+    def ub(self) -> np.ndarray:
+        return self._griewank.bounds[1]
+
+    def dim(self) -> int:
+        return self._dim
+
+
+class Rover(OptimizationProblem):
+
+    def __init__(self, dim: int):
+        def l2cost(x, point):
+            return 10 * np.linalg.norm(x - point, 1)
+
+        domain: RoverDomain = create_large_domain(
+            force_start=False,
+            force_goal=False,
+            start_miss_cost=l2cost,
+            goal_miss_cost=l2cost,
+        )
+        self._domain = domain
+
+    def __call__(self, x: torch.Tensor):
+        x = x.numpy()
+        if x.ndim == 0:
+            x = np.expand_dims(x, 0)
+        if x.ndim == 1:
+            x = np.expand_dims(x, 0)
+        assert x.ndim == 2
+        costs = [-self._domain(y) for y in x]
+
+        costs = np.array(costs).squeeze()
+        return torch.tensor(costs)
+
+    def lb(self) -> np.ndarray:
+        return torch.zeros(60)
+
+    def ub(self) -> np.ndarray:
+        return torch.ones(60)
+
+    def dim(self) -> int:
+        return 60
